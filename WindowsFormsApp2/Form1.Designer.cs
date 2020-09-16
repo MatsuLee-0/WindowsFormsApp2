bf5ac00862503053c675692186ed47d92ad1e894
Alien <Alien@DESKTOP-K4QO84G>
2020-09-16 14:48:17 +0800

    添加项目文件。

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.BackColor = System.Drawing.SystemColors.Window;
+            this.lblDisplay.Location = new System.Drawing.Point(346, 173);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(0, 12);
+            this.lblDisplay.TabIndex = 0;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(348, 203);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "显示\r\n";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lblDisplay);
+            this.Name = "Form1";
+            this.Text = "Welcome";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
